ee61641641904eea94092f150045f73c671b2e6d
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2023-07-18 17:01:35 -0400

    Update App.jsx
    Move the map from App to a separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
-import { Loader } from '@googlemaps/js-api-loader'
 import './App.css'
+import GoogleMap from './components/GoogleMap';
 
 function App() {
-  let center = { lat: 39.152007245589185, lng: -84.51562682302536 };
-  const zoom = 14;
-  const loader = new Loader({
-    apiKey: "AIzaSyCHgrOkqzasqmym75emtsg0JppEb-Ew56c",
-    version: "weekly",
-  })
-
-  let map;
-
-  loader.load().then(async () => {
-    const { Map } = await google.maps.importLibrary("maps");
-    map = new Map(document.getElementById("map"), { center, zoom })
-    map.data.loadGeoJson('clifton.geojson');
-  })
-
   return (
-    <div id="map">
-    </div>
+    <>
+      <main>
+        <GoogleMap />
+      </main>
+    </>
   )
 }
 
