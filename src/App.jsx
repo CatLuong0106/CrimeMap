246a57766d90426ad988169e56e58c089fd5f168
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2023-07-20 00:43:45 -0400

    Update App.jsx
    Set up map color change functionality between map component and dashboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import './App.css'
+import { useState } from 'react';
 import GoogleMap from './components/GoogleMap';
 import NavBar from './components/NavBar';
+import Dashboard from './components/Dashboard';
+import getRandomColor from './utils/RandomColor';
 
 function App() {
+  const [mapThemeColor, setMapThemeColor] = useState('black');
+
+  function parentOnClickHandler() {
+    console.log("Change color button clicked")
+    setMapThemeColor(getRandomColor());
+  }
+  
   return (
     <>
       <NavBar />
       <main>
-        <GoogleMap />
+        <GoogleMap mapThemeColor={mapThemeColor}/>
+        <Dashboard parentOnClickHandler={parentOnClickHandler}/>
       </main>
     </>
   )
