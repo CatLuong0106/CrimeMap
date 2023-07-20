2d20da15a26625efdd9366a457499bb5ddcbc3ab
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2023-07-20 00:36:22 -0400

    Update NavBar.jsx
    Fix class -> className

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,9 +4,9 @@ const NavBar = () => {
     return (
         <nav>
             <div id="title"><a href="/">Crime Map</a></div>
-            <div class="others">
-                <div class="link"><a href="/">about</a></div>
-                <div class="link"><a href="/">author</a></div>
+            <div className="others">
+                <div className="link"><a href="/">about</a></div>
+                <div className="link"><a href="/">author</a></div>
             </div>
         </nav>
     );
