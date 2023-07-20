4313338ead6ed8109f2bcf405ad3cd60317f8bf7
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2023-07-20 00:42:00 -0400

    Update GoogleMap.jsx
    Create a state to store map, and a state to store all features. Create a function to change map color. Create function to hide map points. Create a function to init map

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -1,19 +1,50 @@
-import { Loader } from '@googlemaps/js-api-loader'
+import { Loader } from '@googlemaps/js-api-loader';
+import { useEffect, useState } from 'react';
 
-const GoogleMap = () => {
-    const center = { lat: 39.15, lng: -84.52 };
-    const zoom = 14;
-    const loader = new Loader({
-        apiKey: "AIzaSyCHgrOkqzasqmym75emtsg0JppEb-Ew56c",
-        version: "weekly",
-    })
-    let map;
 
-    loader.load().then(async () => {
-        const { Map } = await google.maps.importLibrary("maps");
-        map = new Map(document.getElementById("map"), { center, zoom })
-        map.data.loadGeoJson('clifton.geojson');
-    })
+const GoogleMap = ({ mapThemeColor }) => {
+    const [features, setFeatures] = useState([]);
+    const [map, setMap] = useState(initMap);
+
+    useEffect(() => {
+        changeThemeColorOfAll(mapThemeColor);
+    }, [mapThemeColor])
+
+    function initMap()  {
+        const center = { lat: 39.15, lng: -84.52 };
+        const zoom = 14;
+        const loader = new Loader({
+            apiKey: import.meta.env.VITE_GOOGLE_MAP_API_KEY,
+            version: "weekly",
+        })
+        
+        let newMap;
+        loader.load().then(async () => {
+            const { Map } = await google.maps.importLibrary("maps");
+            newMap = new Map(document.getElementById("map"), { center, zoom })
+            newMap.data.loadGeoJson('clifton.geojson');
+            newMap.data.setStyle(computeFeature)
+            setMap(newMap);
+        })
+    }    
+
+    function computeFeature(feature) {
+        setFeatures(prevFeatures => prevFeatures.concat([feature]))
+        let type = feature.j.split("/")[0]
+        let visible = type != "node";
+        return { visible }
+    }
+
+    function changeThemeColorOfAll(newColor) {
+        console.log("Changing color of all map features")
+        for (const feature of features) {
+            map.data.overrideStyle(feature, {
+                fillColor: newColor,
+                strokeColor: newColor,
+            })
+        }
+    }
+
 
     return <div id="map"></div>
 }
