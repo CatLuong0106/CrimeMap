c420bc9fc1b2ee0e52243cc0cdb5f6499a4e9d39
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2023-07-26 19:02:37 -0400

    Update GoogleMap.jsx
    Import map context

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -1,49 +1,12 @@
-import { Loader } from '@googlemaps/js-api-loader';
-import { useEffect, useState } from 'react';
+import { useMap } from '../contexts/MapContext';
+import { useEffect } from 'react';
 
-
-const GoogleMap = ({ mapThemeColor }) => {
-    const [features, setFeatures] = useState([]);
-    const [map, setMap] = useState(initMap);
+const GoogleMap = () => {
+    const { initMap } = useMap();
 
     useEffect(() => {
-        changeThemeColorOfAll(mapThemeColor);
-    }, [mapThemeColor])
-
-    function initMap()  {
-        const center = { lat: 39.15, lng: -84.52 };
-        const zoom = 14;
-        const loader = new Loader({
-            apiKey: import.meta.env.VITE_GOOGLE_MAP_API_KEY,
-            version: "weekly",
-        })
-        
-        let newMap;
-        loader.load().then(async () => {
-            const { Map } = await google.maps.importLibrary("maps");
-            newMap = new Map(document.getElementById("map"), { center, zoom })
-            newMap.data.loadGeoJson('clifton.geojson');
-            newMap.data.setStyle(computeFeature)
-            setMap(newMap);
-        })
-    }    
-
-    function computeFeature(feature) {
-        setFeatures(prevFeatures => prevFeatures.concat([feature]))
-        let type = feature.j.split("/")[0]
-        let visible = type != "node";
-        return { visible }
-    }
-
-    function changeThemeColorOfAll(newColor) {
-        console.log("Changing color of all map features")
-        for (const feature of features) {
-            map.data.overrideStyle(feature, {
-                fillColor: newColor,
-                strokeColor: newColor,
-            })
-        }
-    }
+        initMap()
+    }, [])
 
 
     return <div id="map"></div>
