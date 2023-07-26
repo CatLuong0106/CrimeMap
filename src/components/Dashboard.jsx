cb45cef430689581692448c9309764fca958968b
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2023-07-26 19:03:09 -0400

    Update Dashboard.jsx
    Import map context and add draw mode toggle

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useMap } from '../contexts/MapContext';
+import getRandomColor from '../utils/RandomColor';
 
-const Dashboard = ({ parentOnClickHandler }) => {
+const Dashboard = () => {
+    const { changeThemeColorOfAll, setDrawMode } = useMap();
+
+    const onChangeColor = () => { changeThemeColorOfAll(getRandomColor()); }
+    const onDrawModeChange = (event) => {
+        setDrawMode(event.target.value)
+    }
+
     return (
         <div id="dashboard">
-            <input type="range" min="1" max="10" />
-            <button onClick={parentOnClickHandler} >Change color</button>
-            <fieldset>
-                <legend>Pick an option</legend>
 
-                <div className="option">
-                    <input type="checkbox" id="scales" name="scales" />
-                    <label htmlFor="scales">Option 1</label>
-                </div>
-
-                <div className="option">
-                    <input type="checkbox" id="horns" name="horns" />
-                    <label htmlFor="horns">Option 2</label>
-                </div>
-            </fieldset>
-
+            <button onClick={onChangeColor} >Change color</button>
+            <div onChange={onDrawModeChange}>
+                <div><input type="radio" name="drawmode" value="None" /> Disable drawing mode</div>
+                <div><input type="radio" name="drawmode" value="Point" /> Draw Points</div>
+                <div><input type="radio" name="drawmode" value="LineString" /> Draw Lines</div>
+                <div><input type="radio" name="drawmode" value="Polygon" /> Draw Polygon</div>
+            </div>
         </div>
     );
 }
