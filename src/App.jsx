f89eb4464574512223307d814c8d585dfc85df37
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2023-07-26 19:02:05 -0400

    Update App.jsx
    Wrap context provider for map

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,18 @@
 import './App.css'
-import { useState } from 'react';
 import GoogleMap from './components/GoogleMap';
 import NavBar from './components/NavBar';
 import Dashboard from './components/Dashboard';
-import getRandomColor from './utils/RandomColor';
+import { MapProvider } from './contexts/MapContext';
 
-function App() {
-  const [mapThemeColor, setMapThemeColor] = useState('black');
-
-  function parentOnClickHandler() {
-    console.log("Change color button clicked")
-    setMapThemeColor(getRandomColor());
-  }
-  
+function App() {  
   return (
-    <>
+    <MapProvider>
       <NavBar />
       <main>
-        <GoogleMap mapThemeColor={mapThemeColor}/>
-        <Dashboard parentOnClickHandler={parentOnClickHandler}/>
+        <GoogleMap />
+        <Dashboard />
       </main>
-    </>
+    </MapProvider>
   )
 }
 
