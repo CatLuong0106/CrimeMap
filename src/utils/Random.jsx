a773460ba890c4264a5dcb247a48c5c03deafe4a
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2023-10-12 17:13:44 -0400

    Create Random.jsx
    Randomizer util file

---
+++ src/utils/Random.jsx
@@ -0,0 +1,14 @@
+export const getRandomColor = () => {
+    let chars = "0123456789ABCDEF"
+    let ans = "#"
+
+    for (let i = 0; i < 6; i++) {
+        let ran = Math.round(Math.random() * 15)
+        ans += chars[ran]
+    }
+    return ans
+}
+
+export const getRandomUniqueId = () => {
+    return (new Date()).toISOString();
+}
