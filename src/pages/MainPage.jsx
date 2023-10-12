54f416e61ef25098e03a042a23d8b9aebeb698be
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2023-10-12 17:12:52 -0400

    Create pages
    Create About, Author, and Main Page

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GoogleMap from '../components/GoogleMap';
+import Dashboard from '../components/Dashboard';
+
+const MainPage = () => {
+    return (
+        <>
+            <GoogleMap />
+            <Dashboard />
+        </>
+    );
+}
+
+export default MainPage;
