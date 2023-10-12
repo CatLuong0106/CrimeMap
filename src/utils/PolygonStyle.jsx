b2440b1ebedb33d6496de7081c95271783f585e2
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2023-10-12 17:14:12 -0400

    Create PolygonStyle.jsx
    Util file for storing styles of map polygons

---
+++ src/utils/PolygonStyle.jsx
@@ -0,0 +1,20 @@
+export const baseStyle = {
+    fillColor: 'black',
+    strokeColor: 'black',
+    strokeWeight: 2,
+    zIndex: 1,
+}
+
+export const hoveredStyle = {
+    fillColor: 'white',
+    strokeColor: 'white',
+    strokeWeight: 2,
+    zIndex: 2,
+}
+
+export const selectedStyle = {
+    fillColor: 'red',
+    strokeColor: 'red',
+    strokeWeight: 3,
+    zIndex: 2,
+}
