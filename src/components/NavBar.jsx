e288dae41c7a45deef4e340d8e174bed37bfde4b
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2023-10-12 17:21:31 -0400

    Update NavBar.jsx
    Replace anchor with Link tags

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <nav>
-            <div id="title"><a href="/">Crime Map</a></div>
+            <div id="title"><Link to="/">Crime Map</Link></div>
             <div className="others">
-                <div className="link"><a href="/">about</a></div>
-                <div className="link"><a href="/">author</a></div>
+                <div className="link"><Link to="/about">about</Link></div>
+                <div className="link"><Link to="/author">author</Link></div>
             </div>
         </nav>
     );
